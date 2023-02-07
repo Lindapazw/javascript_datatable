2870b8bd1442370a77aa7e189f042d0fa666fe92
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-07 11:45:51 -0300

    fix Math.ceil

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ class DataTable{
             const cells = [ ... tr.children];
 
             const item = {
-                id: this.generateUUID(),
+                id: this.generateUUID(), // genera un ID 
                 values: []
             };
 
@@ -62,10 +62,104 @@ class DataTable{
         }); 
 
         console.log(this.items);
+
+        this.makeTable(); 
+    }
+
+    makeTable(){
+        this.copyItems = [... this.items];
+
+        this.initPagination(this.items.length, this.numberOfEntries);
+
+        const container = document.createElement('div');
+        container.id = this.element.id;
+        this.element.innerHTML = '';
+        this.element.replaceWith(container); // remplazo la tabla con el nuevo div
+
+        // definicion de funciones
+        this.createHTML();
+        this.renderHeaders();
+        this.renderRows();
+        this.renderPagesButtons();
+        this.renderHeadersButtons();
+        this.renderSearch();  // dibuja la busqueda
+        this.renderSelectEntries(); // dibuja los elementos seleccionados
+    }
+
+    initPagination(total, entries){
+        this.pagination.total = total;
+        this.pagination.noItemsPerPage = entries;
+        this.pagination.noPages = Math.ceil(this.pagination.total / this.pagination.noItemsPerPage);
+        this.pagination.actual = 1;
+        this.pagination.pointer = 0;
+        this.pagination.diff = this.pagination.noItemsPerPage - (this.pagination.total % this.pagination.noItemsPerPage)
     }
 
     generateUUID(){
         return (Date.now() * Math.floor(Math.random() * 100000)).toString();
+    }
+
+    createHTML(){
+        this.element.innerHTML = `
+        <div class="datatable-container">
+            <div class= "header-tools">
+                <div class="tools">
+                    <ul class="header-buttons-container">
+                    </ul>
+                </div>
+                <div class="search">
+                    <input type="text" class="search-input">
+                </div>
+            </div>
+            <table class="datatable">
+                <thead>
+                    <tr>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+            <div class="footer-tools">
+                <div class="list-items">
+                    Show
+                    <select name="n-entries" id="n-enties" class="n-entries">
+                    </select>
+                    entries
+                </div>
+
+                <div class="pages">
+                <div>
+            </div>
+        </div>
+        `;
+    }
+
+    renderHeaders(){
+        this.element.querySelector('thead tr').innerHTML = '';
+
+        this.headers.forEach(header => {
+            this.element.querySelector('thead tr').innerHTML = `<tr>${header}</tr>`;
+        });
+    }
+
+    renderRows(){
+
+    }
+
+    renderPagesButtons(){
+
+    }
+
+    renderHeadersButtons(){
+
+    }
+
+    renderSearch(){
+
+    }
+
+    renderSelectEntries(){
+
     }
 }
 
